0e7a74879fdffe8942c2a423df90e9d8ba620901
Daniel Jung <djanimelover18@gmail.com>
2014-01-25 17:04:37 -0500

    Added LevelManager and PlayerController scripts.

---
+++ monkgame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 30f;
+	public float maxSpeed = 5f;
+	public float jumpPower = 500f;
+	public GameObject interactableObject;
+	public AnimalType animalType = AnimalType.TYPE_NONE;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void UsePower()
+	{
+		if(animalType == AnimalType.TYPE_NONE)
+		{
+			// Nothing at all
+		}
+		else if(animalType == AnimalType.TYPE_CAT)
+		{
+		}
+		else if(animalType == AnimalType.TYPE_MOUSE)
+		{
+		}
+		else if(animalType == AnimalType.TYPE_FISH)
+		{
+		}
+		else if(animalType == AnimalType.TYPE_BIRD)
+		{
+		}
+	}
+
+	public void Interact()
+	{
+		if(interactableObject != null)
+		{
+			animalType = interactableObject.GetComponent<AnimalController>().animalType;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "LivingObject")
+		{
+			interactableObject = other.gameObject;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if(other.tag == "LivingObject")
+		{
+			interactableObject = null;
+		}
+	}
+}
