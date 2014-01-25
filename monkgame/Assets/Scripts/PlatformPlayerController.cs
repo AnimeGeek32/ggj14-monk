cc34bf3f648adc7bf2d17dfb6592ad3de9025c97
Daniel Jung <djanimelover18@gmail.com>
2014-01-25 11:22:35 -0500

    Added platformer demo scene. Added Ground layer. Added Physics2D Materials folder with a nonslip material. Added PlatformPlayerCopntroller script.

---
+++ monkgame/Assets/Scripts/PlatformPlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformPlayerController : MonoBehaviour {
+	public float walkSpeed = 30.0f;
+	public float maxSpeed = 5f;
+	public float jumpPower = 1000.0f;
+	public Transform groundTransform;
+
+	private bool isGrounded = false;
+	private bool hasJumped = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isGrounded = Physics2D.Linecast (transform.position, groundTransform.position, 1 << LayerMask.NameToLayer("Ground"));
+		if(Input.GetButtonDown("Jump"))
+		{
+			if(isGrounded)
+				hasJumped = true;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxis("Horizontal");
+
+		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+		if(h * rigidbody2D.velocity.x < maxSpeed)
+			// ... add a force to the player.
+			rigidbody2D.AddForce(Vector2.right * h * walkSpeed);
+		
+		// If the player's horizontal velocity is greater than the maxSpeed...
+		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
+			// ... set the player's velocity to the maxSpeed in the x axis.
+			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+
+		if(hasJumped && isGrounded)
+		{
+			rigidbody2D.AddForce(new Vector2(0, jumpPower));
+			hasJumped = false;
+		}
+	}
+}
