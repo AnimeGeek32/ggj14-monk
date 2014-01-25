7ef8eb91b9df467c92f127686b01c36a54438831
Matthew Murphy <mashumafi@gmail.com>
2014-01-25 16:12:36 -0500

    door controller made

---
+++ monkgame/Assets/Scripts/DoorController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+class DoorController : MonoBehaviour {
+	public string sceneName;
+	public int exitPointId;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		GameManager.Instance.exitPointID = exitPointId;
+		Application.LoadLevel (sceneName);
+	}
+}
