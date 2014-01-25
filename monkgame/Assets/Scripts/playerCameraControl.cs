6f35f588e6c74a4c37cd9aac52a67f7eef3b1dc1
Shane Pisko <shanepisko@gmail.com>
2014-01-25 18:28:57 -0500

    more level stuff

---
+++ monkgame/Assets/Scripts/playerCameraControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerCameraControl : MonoBehaviour {
+	public GameObject targetObject;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 newPosition = transform.position;
+		targetObject = GameObject.FindGameObjectWithTag("Player");
+		newPosition.x = targetObject.transform.position.x;
+		newPosition.y = targetObject.transform.position.y;
+		transform.position = newPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(targetObject != null)
+		{
+			Vector3 newPosition = transform.position;
+			newPosition.x = targetObject.transform.position.x;
+			newPosition.y = targetObject.transform.position.y;
+			transform.position = newPosition;
+		}
+	}
+}
