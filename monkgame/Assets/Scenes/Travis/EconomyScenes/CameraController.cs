aa93efd6c4ad2af14a545825937d3424eee6aef2
travis <travis.liskey@gmail.com>
2014-01-25 20:28:14 -0500

    adding my materials before leaving for safe keeping

---
+++ monkgame/Assets/Scenes/Travis/EconomyScenes/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public float CatchUpSpeed = 5f;
+
+    private GameObject thePlayer;
+    private float y;
+
+    // Use this for initialization
+    void Start()
+    {
+        thePlayer = GameObject.FindGameObjectWithTag("Player");
+        y = gameObject.transform.position.y;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 following = Vector3.Lerp(transform.position, thePlayer.transform.position, Time.deltaTime * CatchUpSpeed);
+        if (following != null || following != Vector3.zero)
+        {
+            transform.position = new Vector3(following.x, y, following.z);
+        }
+    }
+}
---
+++ monkgame/Assets/Scenes/Travis/EconomyScenes/fish.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fish : MonoBehaviour
+{
+
+    public float CatchUpSpeed = 0.2f;
+    public bool Right = true;
+    private GameObject thePlayer;
+    private float y;
+    // Use this for initialization
+    void Start()
+    {
+        thePlayer = GameObject.FindGameObjectWithTag("Player");
+        y = gameObject.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 following = Vector3.Lerp(transform.position, thePlayer.transform.position, Time.deltaTime * CatchUpSpeed);
+        if (following != null || following != Vector3.zero)
+        {
+            transform.position = new Vector3(following.x, y, following.z);
+        }
+    }
+}
