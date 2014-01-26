df1fe154511b6fd3dc30d4069dbf432f0b8feed5
travis <travis.liskey@gmail.com>
2014-01-25 22:52:57 -0500

    prototype of the push over rock

---
+++ monkgame/Assets/PushOverRock.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushOverRock : MonoBehaviour {
+
+    public float StrengthRequirement;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (StrengthRequirement != null && other.CompareTag("Player"))
+        {
+            if (PlayerController.Current.Strength) // need to add public strength to player
+            {
+                gameObject.rigidbody2D.AddForce(new Vector2(4f, 4f));
+            }
+        }
+    }
+}
